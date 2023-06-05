3dfb7ae56686ac1ce800ede6e1b9775cdfef5049
Argenis Osorio <argenisosorio580@hotmail.com>
2023-06-05 15:41:29 -0400

    Avances de consumir de un api pero no se genera el apk todavía

--- app/src/main/java/com/example/helloworldkotlin/MainActivity.kt
+++ app/src/main/java/com/example/helloworldkotlin/MainActivity.kt
@@ -1,11 +1,62 @@
-package com.example.helloworldkotlin
-
-import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import com.example.helloworldkotlin.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
+
+        // Realizar la solicitud a la API en segundo plano
+        FetchDataAsyncTask().execute()
+    }
+
+    inner class FetchDataAsyncTask : AsyncTask<Void, Void, Int>() {
+        override fun doInBackground(vararg params: Void?): Int {
+            var result = 0
+            try {
+                val url = URL("https://sinidot.fundavene.gob.ve/api/countdonor")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.connectTimeout = 5000
+                connection.readTimeout = 5000
+                connection.connect()
+
+                val responseCode = connection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                    val response = StringBuilder()
+                    var line: String?
+                    while (reader.readLine().also { line = it } != null) {
+                        response.append(line)
+                    }
+                    reader.close()
+
+                    val jsonResponse = JSONObject(response.toString())
+                    result = jsonResponse.getInt("result")
+                }
+                connection.disconnect()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: Int) {
+            // Actualizar el contenido del TextView con el número obtenido de la API
+            textView.text = "Hello World $result"
+        }
     }
 }
