7266f9e87afa023b71bcb8d336890ee6a856f77e
Argenis Osorio <argenisosorio580@hotmail.com>
2023-06-05 12:55:12 -0400

    Ajustes básicos en la app para mostrar un portal web como componente WebView

--- app/src/main/java/com/example/helloworldkotlin/MainActivity.kt
+++ app/src/main/java/com/example/helloworldkotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.helloworldkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+
+        // Habilita JavaScript en el WebView si es necesario
+        webView.settings.javaScriptEnabled = true
+
+        // Carga la URL en el WebView
+        webView.loadUrl("https://murachi.cenditel.gob.ve")
     }
 }
